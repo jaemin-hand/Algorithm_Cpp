bd0e430f4c734174543a1bea78f03eaa06007b38
jaemin-hand <thswoalsrns@naver.com>
2025-11-19 03:26:35 +0900

    11.19 study

---
+++ study/study11.19.cpp
@@ -0,0 +1,23 @@
+#include<iostream> // cin cout
+#include<vector>
+#include<utility> // pair 를 쓰기 위한 헤더 (vector 헤더 있으면 생략 가능)
+int main(){
+
+    // Python 튜플느낌.
+    std::pair<int,int> p;
+    p = {10, 20};
+    std::cout << p.first << " " << p.second << std::endl;
+    
+    std::vector<int> v = {1,2,3,4}; // 정수형 담을 수 있는 int list.
+    // Python 문법
+    // for i in list:
+    // auto 로 해서 C++에서 타입 auto로 맞추기
+    for(auto val : v) {
+        std::cout << val << " ";
+    }
+    // 결과출력 : 1 2 3 4 5
+
+    
+
+    return 0;
+}
