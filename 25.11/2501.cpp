17c5b0f601f6a7a7b8657b151f25cd623ff7408c
jaemin-hand <thswoalsrns@naver.com>
2025-11-21 23:57:33 +0900

    11.21 Algorithm

---
+++ 25.11/2501.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+   int N = 0, K = 0, count = 0;
+   cin >> N >> K;
+
+   vector<int> arr;
+
+   for (int i = 1; i <= N; i++) {
+      if (i % N == 0) {
+         arr.push_back(i);
+      }
+   }
+
+   cout << arr[K];
+
+   return 0;
+}
