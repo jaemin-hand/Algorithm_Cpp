4be2b46d575a03d18bba47abb6e30aad3ccccaf2
손재민 <thswoalsrns@naver.com>
2025-11-24 19:52:37 +0900

    2581 bool

--- 25.11/2581.cpp
+++ 25.11/2581.cpp
@@ -2,18 +2,37 @@
 #include<vector>
 #include<cstdio>
 #include<algorithm>
+#include<numeric>
 using namespace std;
+
+bool check(int value) {
+    if(value == 1) return false;
+    for(int i = 2 ; i < value; i++){
+        if(value % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
     freopen("../input.txt","r",stdin);
     int M = 0, N = 0;
     cin >> M >> N;
-    vector<int> v;
-    
+    vector<int> primes;
+
     for(int i = M; i <= N; i++) {
-        v.push_back(i);
+        if(check(i) == true) {
+            primes.push_back(i);
+        }
     }
 
-    for(auto v_value : v) cout << v_value;
+    if(primes.size() > 0) {
+        cout << accumulate(primes.begin(),primes.end(),0) << endl << primes[0];
+    } else {
+        cout << -1;
+    }
+
 
     return 0;
 }
