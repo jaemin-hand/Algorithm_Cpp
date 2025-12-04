1e0da188c34351ff993ffc916a75269b164a02a5
손재민 <thswoalsrns@naver.com>
2025-12-04 17:48:15 +0900

    5073

--- 25.12/5073.cpp
+++ 25.12/5073.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int main() {
     freopen("../input.txt","r",stdin);
-    while(1){
+    while (1) {
+        
         int a = 0, b = 0, c = 0;
         cin >> a >> b >> c;
-        if(a == 0 && b == 0 && c == 0) {
+        if (a == 0 && b == 0 && c == 0) {
             break;
         }
-        if(a == b && b == c && c == a) {
-            cout << "Equilateral" << endl;
-        }
-        else if(a == b && b != c && a == c) {
-            cout << "Isosceles" << endl;
-        }
-        else if(a != b && b == c && a == c) {
-            cout << "Isosceles" << endl;
-        }
-        else if(a == b && b == c && a != c) {
-            cout << "Isosceles" << endl;
-        }
-        else if(a != b && b != c && a != c) {
+        vector<int> compare;
+        compare.push_back(a);
+        compare.push_back(b);
+        compare.push_back(c);
+        sort(compare.begin(), compare.end());
+        if (compare[2] >= compare[0] + compare[1]) {
             cout << "Invalid" << endl;
         }
-
+        else if (compare[0] == compare[1] && compare[1] == compare[2]) {
+            cout << "Equilateral" << endl;
+        }
+        else if (compare[0] == compare[1] || compare[1] == compare[2]) {
+            cout << "Isosceles" << endl;
+        }
+        else {
+            cout << "Scalene" << endl;
+        }
     }
 
 
