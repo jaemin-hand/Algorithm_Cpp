b9dac352befec7efd66ca4c866d335943ad9aef9
손재민 <thswoalsrns@naver.com>
2026-01-08 21:35:10 +0900

    2751 vector_v clear

---
+++ 26.01/2751_v2.cpp
@@ -0,0 +1,39 @@
+// https://www.acmicpc.net/problem/2751
+// 문제
+// N개의 수가 주어졌을 때,
+// 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다.
+// 둘째 줄부터 N개의 줄에는 수가 주어진다.
+// 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다.
+// 수는 중복되지 않는다.
+
+// 출력
+// 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를
+// 한 줄에 하나씩 출력한다.
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N = 0;
+    cin >> N;
+    vector<int> v(N);
+    for(int i = 0 ;i < N; i++){
+        cin >> v[i];
+    }
+
+    sort(v.begin(),v.end());
+
+    for(int i = 0; i < v.size();i++){
+        cout << v[i] << "\n";
+    }
+
+
+    return 0;
+}
