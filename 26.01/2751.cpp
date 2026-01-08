a67d1390386092ea7358da669f15769c274e6066
손재민 <thswoalsrns@naver.com>
2026-01-08 21:30:12 +0900

    2751 array new delete clear

---
+++ 26.01/2751.cpp
@@ -0,0 +1,42 @@
+// https://www.acmicpc.net/problem/2751
+// 문제
+// N개의 수가 주어졌을 때,
+// 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다.
+// 둘째 줄부터 N개의 줄에는 수가 주어진다.
+// 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다.
+// 수는 중복되지 않는다.
+
+// 출력
+// 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를
+// 한 줄에 하나씩 출력한다.
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+    freopen("../input.txt","r",stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N = 0;
+    cin >> N; // N 은 줄의 개수
+    
+    int* array = new int[N];
+
+    for(int i = 0; i < N;i++){
+        cin >> array[i];
+    }
+    
+    sort(array, array + N);
+    
+    for(int i = 0; i < N;i++){
+        cout << array[i] << "\n";
+    }
+    delete[] array;
+
+    return 0;
+}
