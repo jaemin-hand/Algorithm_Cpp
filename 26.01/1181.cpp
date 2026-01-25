98ac9cd88e9586393fafd07d3e057aeae0c0ad91
jaemin-hand <thswoalsrns@naver.com>
2026-01-26 04:43:30 +0900

    1181 sort clear

---
+++ 26.01/1181.cpp
@@ -0,0 +1,52 @@
+https://www.acmicpc.net/problem/1181
+
+// 문제
+// 알파벳 소문자로 이루어진 N개의 단어가 들어오면
+// 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+// 길이가 짧은 것부터
+// 길이가 같으면 사전 순으로
+// 단, 중복된 단어는 하나만 남기고 제거해야 한다.
+
+// 입력
+// 첫째 줄에 단어의 개수 N이 주어진다.
+// (1 ≤ N ≤ 20,000) 둘째 줄부터 N개의 줄에 걸쳐 알파벳 소문자로
+// 이루어진 단어가 한 줄에 하나씩 주어진다.
+// 주어지는 문자열의 길이는 50을 넘지 않는다.
+
+// 출력
+// 조건에 따라 정렬하여 단어들을 출력한다.
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool compare(string a, string b) {
+    if(a.length() != b.length()) {
+        return a.length() < b.length();
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N = 0;
+    cin >> N;
+
+    vector<string> v(N);
+    for(int i = 0; i < N;i++){
+        cin >> v[i];
+    }
+
+    sort(v.begin(),v.end(),compare);
+
+    v.erase(unique(v.begin(),v.end()),v.end());
+
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i] << "\n";
+    }
+
+
+    return 0;
+}
